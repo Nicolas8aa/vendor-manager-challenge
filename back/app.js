3f1884b04df133ce8324b6a66bf3521ca92edcff
Nicolas C <nicolascuellar2305@gmail.com>
2023-08-24 08:18:18 -0500

    Back -> setted up cors

--- back/app.js
+++ back/app.js
@@ -2,12 +2,14 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 
 var app = express();
 
 app.use(logger("dev"));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
