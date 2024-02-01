0e6ffdf749769dd10c5138a1afbb7f52037a3d66
Nicolas Cuellar <nicolascuellar2305@gmail.com>
2024-02-01 18:56:01 +0000

    Update ecosystem.config.js with app configurations

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,18 @@
 module.exports = {
-  apps : [{
-    name   : "backend",
-    script : "./back/bin/www"
+  apps: [{
+    name: "backend",
+    cwd: "back",
+    script: "./bin/www",
+    max_memory_restart: "150M",
+    max_restarts: 10,
   },
   {
-   name: "frontend",
-   script: "npm",
-   args: "start",
-   cwd: "front"
+    name: "frontend",
+    script: "npm",
+    args: "start",
+    cwd: "front",
+    max_memory_restart: "100M",
+    max_restarts: 10,
   }
-]
+  ]
 }
